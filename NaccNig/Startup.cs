cc4aca5560d1e220c51cf29dc3fd5a24759a7d13
Chukwunasa Igwe <chooksigwe@outlook.com>
2018-05-05 13:19:36 +0100

    Add project files.

---
+++ NaccNig/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NaccNig.Startup))]
+namespace NaccNig
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
